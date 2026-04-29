7ef2ae36b66d84ef1abb08e9a4c25c61b4babfc8
blowva <blowva509@gmail.com>
2026-04-29 15:38:05 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import { CartProvider } from './context/CartContext'
+import { LocationProvider } from './context/LocationContext'
+// ...
+<LocationProvider>
+  <CartProvider>
     <App />
-  </StrictMode>,
-)
+  </CartProvider>
+</LocationProvider>
