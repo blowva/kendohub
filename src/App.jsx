2d5d7cd15dfaaa649de4bbf898ca48f0924b3277
blowva <blowva509@gmail.com>
2026-04-30 07:35:30 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Shop from './pages/Shop';
 import ProductDetail from './pages/ProductDetail';
 import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
+import Affiliate from './pages/Affiliate';
+import AffiliateAbout from './pages/AffiliateAbout';
 import { About, Account, Policies, NotFound } from './pages/Misc';
 import { CartProvider } from './context/CartContext';
 import { ThemeProvider } from './context/ThemeContext';
@@ -23,6 +25,8 @@ export default function App() {
               <Route path="/product/:slug" element={<ProductDetail />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/checkout" element={<Checkout />} />
+              <Route path="/affiliate" element={<Affiliate />} />
+              <Route path="/affiliate/about" element={<AffiliateAbout />} />
               <Route path="/about" element={<About />} />
               <Route path="/account" element={<Account />} />
               <Route path="/policies" element={<Policies />} />
