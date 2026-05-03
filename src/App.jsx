1bedd9716e5c08fbc09f298f3e8a77b1b20920f8
blowva <blowva509@gmail.com>
2026-05-03 09:29:56 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AdminProductTest from './pages/AdminProductTest';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
